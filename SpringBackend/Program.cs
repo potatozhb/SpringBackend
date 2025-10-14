b008ec80472b12ba7a6b317873ac83a1815f0274
potatozhb <zhaohongbo1980@gmail.com>
2025-10-14 11:22:07 -0700

    Seed data in DB in development env

--- SpringBackend/Program.cs
+++ SpringBackend/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SpringBackend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMemoryCache();
+if (builder.Configuration.GetValue<bool>("UseInMemoryDB") && builder.Environment.IsDevelopment())
+{
+    Console.WriteLine("--> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+}
+else
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +36,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+SeedDb.InitializeDb(app, app.Environment.IsDevelopment());
 
 app.Run();
